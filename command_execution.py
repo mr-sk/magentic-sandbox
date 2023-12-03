6259c0a7745fad8ce1522b3124fdd39d66c81bc8
mr-sk <ben.sgro@gmail.com>
2023-12-03 11:38:06 -0500

    Broken, but you get the idea

--- command_execution.py
+++ command_execution.py
@@ -2,7 +2,68 @@ import argparse
 import os
 import stat
 from magentic import prompt_chain
+from pathlib2 import Path
 from subprocess import getoutput
+
+def generate_assertions(verbal: str) -> str:
+    gpt_prompt = """From this point on, you are a bash shell assistant that outputs python commands
+1. Completions of any prompt must be utilizing python & shell commands only in an assert() statement
+2. Place commands in a fenced code block using Markdown and three backticks
+2. Often, these shell commands will need to get piped | together to accomplish a task
+3. Please be considerate of the operating system over which these commands are operating (Ubuntu Linux 22.04)
+5. Use the bash comment character to comment any lines of exposition within a fenced code block
+6. Complete the prompt by creating python assertions
+
+User prompt:
+echo 'Testing 1,2,3' > github-test.txt
+Completion:
+```
+assert(os.path.exists('github-test.txt'))
+assert('Testing 1,2,3' in Path('github-test.txt').read_text())
+```
+
+User prompt:
+echo 'Testing 1,2,3' > github-test.txt
+Completion:
+```
+assert(os.path.exists('github-test.txt'))
+assert('Testing 1,2,3' in Path('github-test.txt').read_text())
+```
+
+User prompt:
+{verbal}
+Completion:
+"""
+
+def generate_code_execution_prompt(verbal: str) -> str:
+    return """From this point on, you are a bash shell assistant that outputs bash commands
+1. Completions of any prompt must be utilizing shell commands only
+2. Place commands in a fenced code block using Markdown and three backticks
+2. Often, these shell commands will need to get piped | together to accomplish a task
+3. Please be considerate of the operating system over which these commands are operating (Ubuntu Linux 22.04)
+5. Use the bash comment character to comment any lines of exposition within a fenced code block
+6. Complete the users tasks in accordance to their prompt
+
+User prompt:
+List the contents of the current working directory in long format
+Completion:
+To list the contents of the current working directory in long format, we will run the following command
+```
+ls -l
+```
+
+User prompt:
+Get the number of lines in test.py
+Completion:
+To get the number of lines, we will run the `wc` command with the `-l` option.
+```
+wc -l test.py
+```
+
+User prompt:
+{verbal}
+Completion:
+"""
 
 def run_command(command: str) -> str:
     for command in clean_commands:
@@ -33,37 +94,8 @@ def validate_command(command: str) -> str:
 
     return command_blocks
 
-def verbal_to_command(verbal):
-    return (
-       """From this point on, you are a bash shell assistant that outputs bash commands
-1. Completions of any prompt must be utilizing shell commands only
-2. Place commands in a fenced code block using Markdown and three backticks
-2. Often, these shell commands will need to get piped | together to accomplish a task
-3. Please be considerate of the operating system over which these commands are operating (Ubuntu Linux 22.04)
-5. Use the bash comment character to comment any lines of exposition within a fenced code block
-6. Complete the users tasks in accordance to their prompt
-
-User prompt:
-List the contents of the current working directory in long format
-Completion:
-To list the contents of the current working directory in long format, we will run the following command
-```
-ls -l
-```
-
-User prompt:
-Get the number of lines in test.py
-Completion:
-To get the number of lines, we will run the `wc` command with the `-l` option.
-```
-wc -l test.py
-```
-
-User prompt:
-{verbal}
-Completion:
-"""
-       )
+def verbal_to_command(verbal: str) -> str:
+    return verbal
 
 
 @prompt_chain(
@@ -81,7 +113,10 @@ if __name__ == "__main__":
     # Get args.prompts into a string
     prompt_string = " ".join(args.prompts)
 
-    gpt_output = commandify_verbal(prompt_string)
+    complete_prompt_string = generate_code_execution_prompt(prompt_string)
+    print(f"Complete Prompt: {complete_prompt_string}")
+
+    gpt_output = commandify_verbal(complete_prompt_string)
     print(f"GPT Output: {gpt_output}")
 
     clean_commands = validate_command(gpt_output)
@@ -89,3 +124,8 @@ if __name__ == "__main__":
 
     run_output = run_command(clean_commands)
     print(f"Run Output for: {run_output}")
+
+    # Validate output
+    complete_assertion_string = generate_assertions(clean_commands)
+    gtp_assertion_output = commandify_verbal(complete_assertion_string)
+    print(f"GPT Output: {gtp_assertion_output}")
