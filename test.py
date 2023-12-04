8d6bd22a3ee5f8f7b2f64af270c660003f1b6d2e
mr-sk <ben.sgro@gmail.com>
2023-12-04 08:55:04 -0500

    Minor fix

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 from magentic import prompt
 
-@prompt('Add more elite hacker to: {phrase} but keep it to one sentences')
+@prompt('Add more elite hacker to: {phrase} but keep it to one sentence')
 def hackerize(phrase: str) -> str:
     ...
 
 print(hackerize("Hello World!"))
+
 
